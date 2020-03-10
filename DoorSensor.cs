23a300757972c6e0c149325489a3eb11a98094a0
laukvalitet <201807157@post.au.dk>
2020-03-10 12:38:03 +0100

    State machine skeleton

---
+++ DoorSensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab 
+{
+    class DoorSensor : IDoorSensor
+    {
+        public event EventHandler<EventArgs> DoorOpened;
+        public event EventHandler<EventArgs> DoorClosed;
+        public void OnDoorOpened()
+        {
+            DoorOpened?.Invoke(this, EventArgs
+        }
+
+        public void OnDoorClosed()
+        {
+            DoorClosed?.Invoke(this, null);
+        }
+    }
+}
---
+++ StationControl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.States;
+
+namespace Ladeskab
+{
+    class StationControl
+    {
+        public IState VACANT_DOOR_CLOSED_NO_PHONE_CONNECTED { get; set; }
+        public IState VACANT_DOOR_OPEN_NO_PHONE_CONNECTED { get; set; }
+        public IState VACANT_DOOR_OPEN_PHONE_CONNECTED { get; set; }
+        public IState VACANT_DOOR_CLOSED_PHONE_CONNECTED_AWAITING_RFID { get; set; }
+        public IState VACANT_DOOR_CLOSED_PHONE_CONNECTED_CHECKING_RFID { get; set; }
+        public IState OCCUPIED_DOOR_CLOSED_AWAITING_RFID { get; set; }
+        public IState OCCUPIED_DOOR_CLOSED_CHECKING_RFID { get; set; }
+
+        private IState currentState;
+
+        public StationControl()
+        {
+            VACANT_DOOR_CLOSED_NO_PHONE_CONNECTED=new VacantDoorClosedNoPhoneConnected(this);
+            VACANT_DOOR_OPEN_NO_PHONE_CONNECTED=new VacantDoorOpenNoPhoneConnected(this);
+            VACANT_DOOR_OPEN_PHONE_CONNECTED = new VacantDoorOpenPhoneConnected(this);
+            VACANT_DOOR_CLOSED_PHONE_CONNECTED_AWAITING_RFID = new VacantDoorClosedPhoneConnectedAwaitingRFID(this);
+            VACANT_DOOR_CLOSED_PHONE_CONNECTED_CHECKING_RFID = new VacantDoorClosedPhoneConnectedCheckingRFID(this);
+            OCCUPIED_DOOR_CLOSED_AWAITING_RFID = new OccupiedDoorClosedAwaitingRFID(this);
+            OCCUPIED_DOOR_CLOSED_CHECKING_RFID = new OccupiedDoorClosedCheckingRFID(this);
+
+
+            currentState = VACANT_DOOR_CLOSED_NO_PHONE_CONNECTED;
+        }
+
+        public void SetState(IState newState)
+        {
+            this.currentState = newState;
+        }
+
+
+        public void DoorOpenedHandler(object sender, EventArgs e)
+        {
+            currentState.OnDoorOpened();
+        }
+
+        public void DoorClosedHandler(object sender, EventArgs e)
+        {
+            currentState.OnDoorClosed();
+        }
+
+        public void PhoneConnectedHandler(object sender, EventArgs e)
+        {
+            currentState.OnPhoneConnected();
+        }
+
+        public void PhoneDisconnectedHandler(object sender, EventArgs e)
+        {
+            currentState.OnPhoneDisconnected();
+        }
+
+        public void RfidDetectedHandler(object sender, EventArgs e)
+        {
+            currentState.OnRfidDetected();
+        }
+
+    }
+}
